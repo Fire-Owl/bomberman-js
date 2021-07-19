14d54213cb44c14c9a9a72fceb13daa80a84b3d2
acs <a.bederiat@codeur.online>
2021-07-19 11:37:27 +0200

    bordure de map

--- player.js
+++ player.js
@@ -1,51 +1,62 @@
 const player = document.getElementById('player');
 const moveSize = 24;
-var playerWalk = 0;
+let playerWalk = 0;
+
 
 document.addEventListener('keydown', function(event) {
   if (event.code == 'ArrowUp') {
-    playerWalk = playerWalk + 1;
+    if (player.offsetTop >=24){
+      playerWalk = playerWalk + 1;
     if (playerWalk %2 == 1) {
       
       player.style.backgroundImage= "url('../jeux/asset/images/dos.png')";
-      console.log(playerWalk);
      }
      else{
       player.style.backgroundImage= "url('../jeux/asset/images/dos2.png')";
+      
      }
     player.style.top = (player.offsetTop - moveSize) + "px";
+    }
+    
   } else if (event.code == 'ArrowRight') {
-    playerWalk = playerWalk + 1;
-    if (playerWalk %2 == 1) {
-      
-    player.style.backgroundImage= "url('../jeux/asset/images/droite.png')";
-    console.log(playerWalk);
-   }
-   else{
-    player.style.backgroundImage= "url('../jeux/asset/images/droite2.png')";
-   }
-   player.style.left = (player.offsetLeft + moveSize) + "px";
+    if (player.offsetLeft <= 720) {
+      playerWalk = playerWalk + 1;
+      if (playerWalk %2 == 1) {
+        
+      player.style.backgroundImage= "url('../jeux/asset/images/droite.png')";
+     }
+     else{
+      player.style.backgroundImage= "url('../jeux/asset/images/droite2.png')";
+     }
+     player.style.left = (player.offsetLeft + moveSize) + "px";
+    }
+   
   } else if (event.code == 'ArrowDown') {
-    playerWalk = playerWalk + 1;
+    if (player.offsetTop <= 720) {
+      playerWalk = playerWalk + 1;
     if (playerWalk %2 == 1) {
       
       player.style.backgroundImage= "url('../jeux/asset/images/face.png')";
-      console.log(playerWalk);
      }
      else{
       player.style.backgroundImage= "url('../jeux/asset/images/face2.png')";
      }
     player.style.top = (player.offsetTop + moveSize) + "px";
+    console.log(player.style.top);
+    }
+    
   } else if (event.code == 'ArrowLeft') {
-    playerWalk = playerWalk + 1;
+    if (player.offsetLeft >= 24) {
+      playerWalk = playerWalk + 1;
     if (playerWalk %2 == 1) {
       
       player.style.backgroundImage= "url('../jeux/asset/images/gauche.png')";
-      console.log(playerWalk);
      }
      else{
       player.style.backgroundImage= "url('../jeux/asset/images/gauche2.png')";
      }
     player.style.left = (player.offsetLeft - moveSize) + "px";
+    }
+    
   }
 });
